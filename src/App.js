b344c1b66d050d81651d5cb538c820faf44f04d9
Abdul Hadi <31170019+iihadi@users.noreply.github.com>
2023-06-09 13:27:13 +0100

    initial commi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+const Square = (props) => {
+  return (
+    <button 
+      className="square"
+      onClick={props.onClickEvent}
+      >
+      {props.value}
+    </button>
+  )
+}
+
+const Board = () =>{
+  const initialSquares = Array(9).fill(null);
+  const [squares, setSquares] = useState(initialSquares);
+
+  const [xIsNext, setxIsNext] = useState(true);
+
+  
+  const handleClickEvent = (i) => {
+    const newSquares = [...squares];
+    const winnerDeclared = Boolean(calculateWinner(newSquares))
+    const squareFilled = Boolean(newSquares[i]);
+    if (winnerDeclared || squareFilled) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setxIsNext(!xIsNext);
+  
+  }
+
+  const renderSquare = (i) => {
+    return (
+      <Square value={squares[i]}
+       onClickEvent={() => handleClickEvent(i)}
+      />
+    );
+  };
+
+  
+  const winner = calculateWinner(squares);
+  const status = winner ?
+  `The winner is: ${winner}` :
+  `Next player is: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+      {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+      </div>
+      <div className="board-row">
+      {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+      </div>
+      
+    </div>
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      Simple Tic Tac Toe
+      <Board/>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+    [0, 4, 8], [2, 4, 6] //diagonals
+  ]
+
+  for (let line of lines) {
+    const[a, b, c] = line;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]; //Theres a winner
+  }}
+  return null;
 }
 
 export default App;
